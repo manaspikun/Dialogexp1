88adfd6bebb968cf8163a21705797f828caed1db
manasranjan <manasmahanta92@gmail.com>
2016-12-23 15:55:44 +0530

    add customdialog,time picker and date picker

--- app/src/main/java/com/techpalle/dilogexp1/MainActivity.java
+++ app/src/main/java/com/techpalle/dilogexp1/MainActivity.java
@@ -4,9 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button b,b1;
+    Button b,b1,b2,b3,b4;
+    TextView tv;
+    public void catchData(String date){
+        tv.setText("selected:"+date);
+    }
+    public void catchData1(String time){
+        tv.setText("selected:"+time);
+
+    }
 
 
     @Override
@@ -15,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b= (Button) findViewById(R.id.button1);
         b1= (Button) findViewById(R.id.button2);
+        b2= (Button) findViewById(R.id.button3);
+        b3= (Button) findViewById(R.id.button4);
+        b4= (Button) findViewById(R.id.button5);
+
+        tv= (TextView) findViewById(R.id.textview1);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +44,28 @@ public class MainActivity extends AppCompatActivity {
                 //sow alert dialog popup
                 MyDilogFragment myDilogFragment=new MyDilogFragment();
                 myDilogFragment.show(getSupportFragmentManager(),null);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              MyDatePicker myDatePicker=new MyDatePicker();
+                myDatePicker.show(getSupportFragmentManager(),null);
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTimePicker myTimePicker=new MyTimePicker();
+                myTimePicker.show(getSupportFragmentManager(),null);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+           MyCustomDialog myCustomDialog=new MyCustomDialog();
+                myCustomDialog.show(getSupportFragmentManager(),null);
             }
         });
     }
